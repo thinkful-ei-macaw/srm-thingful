87b24654b89760d21e8a90e1cf6c001adea26006
Sara R Mills <SaraRMills@gmail.com>
2020-04-27 17:11:53 -0400

    added secure login

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 function makeUsersArray() {
   return [
@@ -280,10 +281,14 @@ function seedMaliciousThing(db, user, thing) {
     )
 }
 
-function makeAuthHeader(user) {
-  const token = Buffer.from(`${user.user_name}:${user.password}`).toString('base64')
+function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
+  const token = jwt.sign
+  ({ user_id: user.id }, secret, {
+      subject: user.user_name,
+      algorithm: 'HS256'
+    })
 
-  return `Basic ${token}`
+  return `Bearer ${token}`
 }
 
 module.exports = {
